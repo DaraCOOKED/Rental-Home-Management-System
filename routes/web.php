8fde63709ef70b0c81a6dfb14a05163ed36d995d
DaraCOOKED <dara.somnag020@gmail.com>
2026-04-07 13:13:35 +0700

    commit the code in branch login because i already finish the login page.

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/tenant/login', fn() => view('auth.tanent-login'));
 });
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
